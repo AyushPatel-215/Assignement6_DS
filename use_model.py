e9727e8d634abc4e27a67afb65b48595e73ae862
Ayush Patel <ayush.patel@marutitech.com>
2023-06-05 11:31:08 +0530

    Assignment-6 | break parts into chunks
    Assignment-6 | break parts into chunks

--- use_model.py
+++ use_model.py
@@ -1,11 +1,18 @@
 import pickle
 
-with open('model.pickle', 'rb') as file:
-    loaded_model = pickle.load(file)
+def open_file():
+    global loaded_model
+    with open('model.pickle', 'rb') as file:
+        loaded_model = pickle.load(file)
+def take_input_from_user():
+    input_data = input("Enter the values for prediction (comma-separated): ")
+    input_values = list(map(float, input_data.split(",")))
+    # 165349.20, 136897.80, 471784.10, 0, 1
 
-input_data = input("Enter the values for prediction (comma-separated): ")
-input_values = list(map(float, input_data.split(",")))
-# 165349.20, 136897.80, 471784.10, 0, 1
+    loaded_predicated_value = loaded_model.predict([input_values])
+    print("Loaded Predicted Value:", loaded_predicated_value[0])
 
-loaded_predicated_value = loaded_model.predict([input_values])
-print("Loaded Predicted Value:", loaded_predicated_value[0])
+
+if __name__ == '__main__':
+    open_file()
+    take_input_from_user()
